f936a56d73a7aff77650cc5ad8c1a35fd425ae6c
Snehatae <72919390+Snehatae@users.noreply.github.com>
2020-12-03 16:06:06 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,19 @@
+class Box{
+    constructor(x,y,w,h){
+    this.body=Bodies.rectangle(x,y,w,h);
+    World.add(world,this.body);
+    this.width=w;
+    this.height=h;
+    }
+display(){
+push();
+translate(this.body.position.x,this.body.position.y);
+angleMode(RADIANS);
+rotate(this.body.angle);
+fill(255,0,0);
+rectMode(CENTER);
+rect(0,0,this.width,this.height);
+
+pop();
+    }
+}
